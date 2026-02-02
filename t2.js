8fb0a72dcf5b0f8cac0309bf9bc83a25110c95ce
inamrana <iaminamrana@gmail.com>
2026-02-02 12:52:56 +0500

    Task2 in progress

---
+++ t2.js
@@ -0,0 +1,18 @@
+function startMoving() {
+    const box = document.getElementById("box");
+    let position=0;
+    const interval = setInterval(moveBox, 10);
+}
+    function moveBox() {
+        if (position >= 350) {
+            clearInterval(interval);
+        }           
+        else {
+            position++;
+            box.style.left = position + 'px';
+        }
+    }
+
+
+
+
