b06be2cf8c500917325500f27605cc0c2e7d7475
inamrana <iaminamrana@gmail.com>
2026-02-08 01:27:32 +0500

    updated styling

--- t2.js
+++ t2.js
@@ -1,16 +1,20 @@
 function startMoving() {
+
     const box = document.getElementById("box");
     let position=0;
     const interval = setInterval(moveBox, 10);
 }
     function moveBox() {
+
         if (position >= 350) {
             clearInterval(interval);
+
         }           
         else {
             position++;
             box.style.left = position + 'px';
         }
+        
     }
 
 
